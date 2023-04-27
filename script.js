03e94454fe23a28eaa6a28bda1377b40311e086a
Anuj Karoddeo <authenticwork2002@gmail.com>
2023-04-27 17:09:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+
+
+// bmi formula weight/height square
+const btn = document.getElementById("calculate");
+
+btn.addEventListener("click", function () {
+  let height = document.querySelector("#height").value;
+  let weight = document.querySelector("#weight").value;
+
+  if (height == "" || weight == "") {
+    alert("Please fill out the input fields! 😉");
+    return;
+  }
+
+  // BMI = weight in KG / (height in m * height in m)
+
+  height = height / 100;
+
+  let BMI = weight / (height * height);
+
+  BMI = BMI.toFixed(2);
+
+  document.querySelector("#result").innerHTML = BMI;
+
+  let status = "";
+
+  if (BMI < 18.5) {
+    status = "Underweight 😋";
+  }
+  if (BMI >= 18.5 && BMI < 25) {
+    status = "Healthy 🥳";
+  }
+  if (BMI >= 25 && BMI < 30) {
+    status = "Overweight 😬";
+  }
+  if (BMI >= 30) {
+    status = "Obese 😫";
+  }
+  document.querySelector(
+    ".comment"
+  ).innerHTML = `Comment: you are <span id="comment">${status}</span>`;
+});
+
+
+
+
+
+
+
+
